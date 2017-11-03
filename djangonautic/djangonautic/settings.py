19be0c4d730dde180381a8b0b40354da4de39ffd
shaunoftheshed pelling <shaunoftheshed pelling>
2017-11-03 11:17:53 +0000

    lesson 4 code

--- djangonautic/djangonautic/settings.py
+++ djangonautic/djangonautic/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'djangonautic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
