5f1008b91759638955570425022979ed0d34be5d
shaunoftheshed pelling <shaunoftheshed pelling>
2017-11-10 10:20:39 +0000

    lesson 6 code

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    # add in thumbnail later
+    # add in author later
