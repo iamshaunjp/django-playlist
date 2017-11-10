dbc1d68dd0fae36333327ba617e3a2de323b0126
shaunoftheshed pelling <shaunoftheshed pelling>
2017-11-10 17:29:44 +0000

    lesson 16 code

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -7,4 +7,6 @@ def article_list(request):
     return render(request, 'articles/article_list.html', { 'articles': articles })
 
 def article_detail(request, slug):
-    return HttpResponse(slug)
+    # return HttpResponse(slug)
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html', { 'article': article })
