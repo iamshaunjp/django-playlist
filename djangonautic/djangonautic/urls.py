a1e2a18e70c6d0c81c25c272cabc3c4a99b9f1b8
shaunoftheshed pelling <shaunoftheshed pelling>
2017-11-10 16:49:17 +0000

    lesson 12 code

--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -8,3 +9,5 @@ urlpatterns = [
     url(r'^about/$', views.about),
     url(r'^$', views.homepage),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
