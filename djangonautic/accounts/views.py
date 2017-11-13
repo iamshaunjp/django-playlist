96253e14a0749920c4afc088e8da9f70b44f3168
shaunoftheshed pelling <shaunoftheshed pelling>
2017-11-13 08:16:51 +0000

    lesson 19 code

--- djangonautic/accounts/views.py
+++ djangonautic/accounts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 def signup_view(request):
-    return render(request, 'accounts/signup.html')
+    form = UserCreationForm()
+    return render(request, 'accounts/signup.html', { 'form': form })
