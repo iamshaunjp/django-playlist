61d29682fa1f4d57bcc8ca1e8352c2973efca1bd
shaunoftheshed pelling <shaunoftheshed pelling>
2017-11-13 12:38:16 +0000

    lesson 24 code

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -5,5 +5,6 @@ app_name = 'articles'
 
 urlpatterns = [
     url(r'^$', views.article_list, name="list"),
+    url(r'^create/$', views.article_create, name="create"),
     url(r'^(?P<slug>[\w-]+)/$', views.article_detail, name="detail"),
 ]
--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Article
+from django.contrib.auth.decorators import login_required
 
 def article_list(request):
     articles = Article.objects.all().order_by('date');
@@ -10,3 +11,7 @@ def article_detail(request, slug):
     # return HttpResponse(slug)
     article = Article.objects.get(slug=slug)
     return render(request, 'articles/article_detail.html', { 'article': article })
+
+@login_required(login_url="/accounts/login/")
+def article_create(request):
+    return render(request, 'articles/article_create.html')
