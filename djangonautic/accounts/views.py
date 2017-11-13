5a2fc6690c2281814cce241bf05fb3fdbfd37afc
shaunoftheshed pelling <shaunoftheshed pelling>
2017-11-13 14:51:05 +0000

    lesson 25 code

--- djangonautic/accounts/views.py
+++ djangonautic/accounts/views.py
@@ -21,7 +21,10 @@ def login_view(request):
             # log the user in
             user = form.get_user()
             login(request, user)
-            return redirect('articles:list')
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+               return redirect('articles:list')
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', { 'form': form })
