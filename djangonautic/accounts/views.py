c509687a8c68b83a6183e261d0f4dba67d42549d
shaunoftheshed pelling <shaunoftheshed pelling>
2017-11-13 11:07:12 +0000

    refactor

--- djangonautic/accounts/views.py
+++ djangonautic/accounts/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
-from django.contrib.auth.models import User
 
 def signup_view(request):
     if request.method == 'POST':
          form = UserCreationForm(request.POST)
          if form.is_valid():
              form.save()
-             #  now log the user in
-             user = User.objects.get(username=form.cleaned_data.get('username'))
-             login(request, user)
+             #  log the user in
              return redirect('articles:list')
     else:
         form = UserCreationForm()
