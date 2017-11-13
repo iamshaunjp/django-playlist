ddce9194fdab7ef75e6eb77609f267647e23e10c
shaunoftheshed pelling <shaunoftheshed pelling>
2017-11-13 12:17:09 +0000

    lesson 22 code

--- djangonautic/accounts/views.py
+++ djangonautic/accounts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 def signup_view(request):
     if request.method == 'POST':
          form = UserCreationForm(request.POST)
          if form.is_valid():
-             form.save()
+             user = form.save()
              #  log the user in
+             login(request, user)
              return redirect('articles:list')
     else:
         form = UserCreationForm()
@@ -17,6 +19,8 @@ def login_view(request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             # log the user in
+            user = form.get_user()
+            login(request, user)
             return redirect('articles:list')
     else:
         form = AuthenticationForm()
