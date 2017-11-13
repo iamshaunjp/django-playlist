0b69aa6dcc2c11935657b30aebd3e508b64a8a3b
shaunoftheshed pelling <shaunoftheshed pelling>
2017-11-13 15:39:19 +0000

    lesson 26 code

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 from django.contrib.auth.decorators import login_required
+from . import forms
 
 def article_list(request):
     articles = Article.objects.all().order_by('date');
@@ -14,4 +15,11 @@ def article_detail(request, slug):
 
 @login_required(login_url="/accounts/login/")
 def article_create(request):
-    return render(request, 'articles/article_create.html')
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid():
+            # save article to db
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle()
+    return render(request, 'articles/article_create.html', { 'form': form })
